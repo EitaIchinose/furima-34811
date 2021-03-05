93f76af4c2a0ed383ed71f1938bb207b6282978a
EitaIchinose <dellps.psdell@gmail.com>
2021-03-05 14:30:32 +0900

    buy・addressモデル作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_cost
   belongs_to :user
+  has_one :buy
   has_one_attached :image
 
   with_options presence: true do
