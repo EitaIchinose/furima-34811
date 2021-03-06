9f9831b26be485a1d24b82b1f2d9896a506fb9a8
EitaIchinose <dellps.psdell@gmail.com>
2021-03-06 20:16:10 +0900

    :authenticate_user!にcreateアクションの追加

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,5 +1,5 @@
 class BuysController < ApplicationController
-  before_action :authenticate_user!, only: :index
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_product, only: [:index, :create]
   before_action :move_to_index, only: [:index, :create]
 
